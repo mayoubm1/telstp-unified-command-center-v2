4bf7a057180b9fa598153d0746f7ba296e65e0fc
mayoubm1 <mayoubm1@example.com>
2025-11-30 19:38:39 -0500

    Fix: Change index.html script to relative path and remove base from vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ import path from 'path'
 export default defineConfig({
 
   plugins: [react()],
-  base: './',
-  resolve: {
+ resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
